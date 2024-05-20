a157fb94dd5113b7d274b70a3c05699dc8902de8
vinhdubai10 <vinhdubai10@gmail.com>
2024-05-20 23:24:36 +0700

    update

--- MyStoreManage/WindowProductManage.xaml.cs
+++ MyStoreManage/WindowProductManage.xaml.cs
@@ -84,7 +84,8 @@ namespace MyStoreManage
             {
                 var selectedProduct = (Product)lvProducts.SelectedItem;
                 selectedProduct.ProductName = txtProductName.Text;
-
+                selectedProduct.UnitPrice = int.Parse(txtUnitPrice.Text);
+                selectedProduct.CategoryId = ((Category)cbbCategory.SelectedItem).CategoryId;
                 try
                 {
                     _context.Products.Update(selectedProduct);
@@ -148,11 +149,41 @@ namespace MyStoreManage
         {
             try
             {
-                string keyword = txtSearch.Text;
-                lvProducts.ItemsSource = _context.Products
+                string productName = txtSearch.Text.Trim();
+                string unitPriceText = txtSearch_Copy.Text.Trim();
+                int? unitPrice = null;
+
+                
+                if (!string.IsNullOrWhiteSpace(unitPriceText))
+                {
+                    unitPrice = int.Parse(unitPriceText);
+                }
+
+                var query = _context.Products
                     .Include(x => x.Category)
-                    .Where(p => p.ProductName.Contains(keyword))
-                    .ToList();
+                    .AsQueryable();
+
+          
+                if (!string.IsNullOrWhiteSpace(productName) && unitPrice.HasValue)
+                {
+                    query = query.Where(p => p.ProductName.Contains(productName) && p.UnitPrice == unitPrice);
+                }
+                else
+                {
+                   
+                    if (!string.IsNullOrWhiteSpace(productName))
+                    {
+                        query = query.Where(p => p.ProductName.Contains(productName));
+                    }
+
+                    if (unitPrice.HasValue)
+                    {
+                        query = query.Where(p => p.UnitPrice == unitPrice);
+                    }
+                }
+
+               
+                lvProducts.ItemsSource = query.ToList();
             }
             catch (Exception ex)
             {
@@ -201,24 +232,24 @@ namespace MyStoreManage
             e.Handled = true;
         }
 
-        private void SearchProduct()
-        {
-            try
-            {
-                string keyword = txtSearch.Text;
-                lvProducts.ItemsSource = _context.Products
-                    .Include(x => x.Category)
-                    .Where(p => p.ProductName.Contains(keyword))
-                    .ToList();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error searching products: {ex.Message}");
-            }
-        }
-        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            SearchProduct();
-        }
+        //private void SearchProduct()
+        //{
+        //    try
+        //    {
+        //        string keyword = txtSearch.Text;
+        //        lvProducts.ItemsSource = _context.Products
+        //            .Include(x => x.Category)
+        //            .Where(p => p.ProductName.Contains(keyword))
+        //            .ToList();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show($"Error searching products: {ex.Message}");
+        //    }
+        //}
+        //private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    SearchProduct();
+        //}
     }
 }
